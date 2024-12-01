1b12e69351b1347cc84ea87001bcafc341de04d5
kingstarfly <xingxiang@twotreesgroup.com>
2024-12-01 22:47:54 +0800

    day 1

--- day-1/a.ts
+++ day-1/a.ts
@@ -1,9 +1,31 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day1a(data: string[]) {
-  console.log(data);
-  return 0;
+  // regex with named groups for better readability
+  const regex = /(?<left>\d+)\s+(?<right>\d+)/;
+
+  const result = data
+    .map((line) => {
+      const match = line.match(regex);
+      if (match?.groups === undefined) {
+        return null;
+      }
+      return {
+        left: parseInt(match.groups.left),
+        right: parseInt(match.groups.right),
+      };
+    })
+    .filter((item) => item !== null);
+
+  const leftList = result.map((item) => item.left).sort((a, b) => a - b);
+  const rightList = result.map((item) => item.right).sort((a, b) => a - b);
+
+  const totalDifference = leftList.reduce((acc, cur, index) => {
+    return acc + Math.abs(cur - rightList[index]);
+  }, 0);
+
+  return totalDifference;
 }
 
 await runSolution(day1a);
--- day-1/b.ts
+++ day-1/b.ts
@@ -1,9 +1,41 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day1b(data: string[]) {
-  console.log(data);
-  return 0;
+  // regex with named groups for better readability
+  const regex = /(?<left>\d+)\s+(?<right>\d+)/;
+
+  const result = data
+    .map((line) => {
+      const match = line.match(regex);
+      if (match?.groups === undefined) {
+        return null;
+      }
+      return {
+        left: parseInt(match.groups.left),
+        right: parseInt(match.groups.right),
+      };
+    })
+    .filter((item) => item !== null);
+
+  const leftList = result.map((item) => item.left);
+  const rightList = result.map((item) => item.right);
+
+  const freqMap = new Map<number, number>();
+  for (const item of rightList) {
+    freqMap.set(item, (freqMap.get(item) ?? 0) + 1);
+  }
+
+  let similarityScore = 0;
+  for (const item of leftList) {
+    const freq = freqMap.get(item);
+    if (freq === undefined) {
+      continue;
+    }
+    similarityScore += freq * item;
+  }
+
+  return similarityScore;
 }
 
 await runSolution(day1b);
