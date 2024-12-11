dda267ab7493d09b6cc28b711c9b51f2785d6e5d
kingstarfly <xingxiang@twotreesgroup.com>
2024-12-11 22:31:26 +0800

    day 11

--- day-11/a.ts
+++ day-11/a.ts
@@ -1,9 +1,40 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day11a(data: string[]) {
-  console.log(data);
-  return 0;
+  if (data.length === 0) return 0;
+  const line = data[0];
+  if (line.length === 0) return 0;
+  const MAX_BLINKS = 25;
+
+  // Get initial stones
+  let previousStones = line.split(" ");
+  let currentStones = [];
+
+  for (let i = 0; i < MAX_BLINKS; i++) {
+    currentStones = [];
+    for (const stone of previousStones) {
+      if (stone === "0") {
+        currentStones.push("1");
+        continue;
+      }
+
+      const numDigits = stone.length;
+      if (numDigits % 2 === 0) {
+        // Split
+        const firstStone = stone.slice(0, numDigits / 2);
+        const secondStone = parseInt(stone.slice(numDigits / 2)).toString();
+        currentStones.push(firstStone);
+        currentStones.push(secondStone);
+      } else {
+        currentStones.push((parseInt(stone) * 2024).toString());
+      }
+    }
+    console.log(currentStones);
+    previousStones = currentStones;
+  }
+
+  return currentStones.length;
 }
 
 await runSolution(day11a);
--- day-11/b.ts
+++ day-11/b.ts
@@ -1,9 +1,64 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day11b(data: string[]) {
-  console.log(data);
-  return 0;
+  if (data.length === 0) return 0;
+  const line = data[0];
+  if (line.length === 0) return 0;
+  const MAX_BLINKS = 25;
+
+  // Lots of repeated cases - we notice that stones get to 0, we will have a definite answer to it?
+  // dp[stone][blinksLeft] = number of stones at the end
+  const previousAnswer = {};
+
+  function calcAndSaveAnswer(stoneText: string, blinksLeft: number) {
+    if (previousAnswer[stoneText]?.[blinksLeft] !== undefined) {
+      return previousAnswer[stoneText][blinksLeft];
+    }
+
+    function saveAnswer(stoneText: string, blinksLeft: number, answer: number) {
+      if (!previousAnswer[stoneText]) {
+        previousAnswer[stoneText] = {};
+      }
+      previousAnswer[stoneText][blinksLeft] = answer;
+    }
+
+    if (blinksLeft === 0) {
+      const answer = 1;
+      saveAnswer(stoneText, blinksLeft, answer);
+      return answer;
+    }
+
+    // Handle each case
+    if (stoneText === "0") {
+      return calcAndSaveAnswer("1", blinksLeft - 1);
+    }
+
+    if (stoneText.length % 2 === 0) {
+      const firstStone = stoneText.slice(0, stoneText.length / 2);
+      const secondStone = parseInt(
+        stoneText.slice(stoneText.length / 2)
+      ).toString();
+      const answer =
+        calcAndSaveAnswer(firstStone, blinksLeft - 1) +
+        calcAndSaveAnswer(secondStone, blinksLeft - 1);
+      saveAnswer(stoneText, blinksLeft, answer);
+      return answer;
+    }
+
+    const answer = calcAndSaveAnswer(
+      (parseInt(stoneText) * 2024).toString(),
+      blinksLeft - 1
+    );
+    saveAnswer(stoneText, blinksLeft, answer);
+    return answer;
+  }
+
+  return line.split(" ").reduce((acc, stoneText) => {
+    const count = calcAndSaveAnswer(stoneText, MAX_BLINKS);
+    console.log({ previousAnswer });
+    return acc + count;
+  }, 0);
 }
 
 await runSolution(day11b);
