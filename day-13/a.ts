ffbc611b62b530bf87a12b032bae259e6832f5df
kingstarfly <xingxiang@twotreesgroup.com>
2024-12-16 22:07:58 +0800

    day13 - LOL linear algebra

--- day-13/a.ts
+++ day-13/a.ts
@@ -1,9 +1,168 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day13a(data: string[]) {
-  console.log(data);
-  return 0;
+  // Push button A = 3 tokens
+  // Push button B = 1 token
+  // Each button push <= 100
+  // For each machine, find the fewest number of tokens needed to win the prize.
+
+  const machineLines = data.reduce<string[][]>((acc, line, index) => {
+    const positionInGroup = index % 4;
+
+    // Skip every 4th line
+    if (positionInGroup === 3) {
+      return acc;
+    }
+
+    // If this is the first line of a group, create a new array
+    if (positionInGroup === 0) {
+      acc.push([line]);
+    } else {
+      // Add to the current group
+      acc.at(-1).push(line);
+    }
+
+    return acc;
+  }, []);
+
+  const MAX_PER_BUTTON_PUSHES = 100;
+  const BUTTON_A_TOKENS = 3;
+  const BUTTON_B_TOKENS = 1;
+  const UNREACHABLE_TOKENS = 999;
+
+  return machineLines
+    .map((lines) => {
+      const buttonRegex = /(\+\d+)/gm;
+      const [xMovementForA, yMovementForA] = Array.from(
+        lines[0].matchAll(buttonRegex)
+      ).map((match) => match[1]);
+      const [xMovementForB, yMovementForB] = Array.from(
+        lines[1].matchAll(buttonRegex)
+      ).map((match) => match[1]);
+
+      const prizeRegex = /(\d+)/gm;
+      const [xPrize, yPrize] = Array.from(lines[2].matchAll(prizeRegex)).map(
+        (match) => match[1]
+      );
+
+      const movementsForA = {
+        x: parseInt(xMovementForA),
+        y: parseInt(yMovementForA),
+      };
+
+      const movementsForB = {
+        x: parseInt(xMovementForB),
+        y: parseInt(yMovementForB),
+      };
+
+      const prizePosition = {
+        x: parseInt(xPrize),
+        y: parseInt(yPrize),
+      };
+
+      return {
+        movementsForA,
+        movementsForB,
+        prizePosition,
+      };
+    })
+    .map((props) => {
+      const prevResults: Map<number, Map<number, number>> = new Map();
+
+      function getMinTokens(
+        prizePosition: {
+          x: number;
+          y: number;
+        },
+        movementsA: {
+          x: number;
+          y: number;
+        },
+        buttonAPushes: number,
+        movementsB: {
+          x: number;
+          y: number;
+        },
+        buttonBPushes: number
+      ): number {
+        const currentPosition = {
+          x: buttonAPushes * movementsA.x + buttonBPushes * movementsB.x,
+          y: buttonAPushes * movementsA.y + buttonBPushes * movementsB.y,
+        };
+        const tokensUsed =
+          BUTTON_A_TOKENS * buttonAPushes + BUTTON_B_TOKENS * buttonBPushes;
+
+        if (
+          currentPosition.x === prizePosition.x &&
+          currentPosition.y === prizePosition.y
+        ) {
+          return tokensUsed;
+        }
+
+        if (
+          buttonAPushes > MAX_PER_BUTTON_PUSHES ||
+          buttonBPushes > MAX_PER_BUTTON_PUSHES
+        ) {
+          return UNREACHABLE_TOKENS;
+        }
+
+        const savedResult = prevResults.get(buttonAPushes)?.get(buttonBPushes);
+        // Check if we have calculated this function before.
+        if (savedResult !== undefined) {
+          console.debug(
+            `[cached] ${buttonAPushes}, ${buttonBPushes} = ${savedResult}`
+          );
+          return savedResult;
+        }
+
+        // Try pressing button A
+        const tokensA = getMinTokens(
+          prizePosition,
+          movementsA,
+          buttonAPushes + 1,
+          movementsB,
+          buttonBPushes
+        );
+
+        // Try pressing button B
+        const tokensB = getMinTokens(
+          prizePosition,
+          movementsA,
+          buttonAPushes,
+          movementsB,
+          buttonBPushes + 1
+        );
+
+        const minTokens = Math.min(tokensA, tokensB);
+
+        // Memoise the result.
+        if (!prevResults.has(buttonAPushes)) {
+          prevResults.set(buttonAPushes, new Map());
+        }
+
+        const prevResultsForButtonA = prevResults.get(buttonAPushes);
+        prevResultsForButtonA.set(buttonBPushes, minTokens);
+
+        // console.debug(`[${buttonAPushes}, ${buttonBPushes}] = ${minTokens}`);
+        return minTokens;
+      }
+
+      const minTokens = getMinTokens(
+        props.prizePosition,
+        props.movementsForA,
+        0,
+        props.movementsForB,
+        0
+      );
+
+      if (minTokens === UNREACHABLE_TOKENS) {
+        return 0;
+      }
+      console.log(minTokens);
+      return minTokens;
+    })
+    .reduce((acc, cur) => acc + cur, 0);
 }
 
 await runSolution(day13a);
--- day-13/b.ts
+++ day-13/b.ts
@@ -1,9 +1,99 @@
-import { runSolution } from '../utils.ts';
+import { lusolve } from "mathjs";
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day13b(data: string[]) {
-  console.log(data);
-  return 0;
+  // Push button A = 3 tokens
+  // Push button B = 1 token
+  // Each button push <= 100
+  // For each machine, find the fewest number of tokens needed to win the prize.
+
+  const machineLines = data.reduce<string[][]>((acc, line, index) => {
+    const positionInGroup = index % 4;
+
+    // Skip every 4th line
+    if (positionInGroup === 3) {
+      return acc;
+    }
+
+    // If this is the first line of a group, create a new array
+    if (positionInGroup === 0) {
+      acc.push([line]);
+    } else {
+      // Add to the current group
+      acc.at(-1).push(line);
+    }
+
+    return acc;
+  }, []);
+
+  const BUTTON_A_TOKENS = 3;
+  const BUTTON_B_TOKENS = 1;
+  const PRIZE_POSITION_EXTRA = 10_000_000_000_000;
+
+  return machineLines
+    .map((lines) => {
+      const buttonRegex = /(\+\d+)/gm;
+      const [xMovementForA, yMovementForA] = Array.from(
+        lines[0].matchAll(buttonRegex)
+      ).map((match) => match[1]);
+      const [xMovementForB, yMovementForB] = Array.from(
+        lines[1].matchAll(buttonRegex)
+      ).map((match) => match[1]);
+
+      const prizeRegex = /(\d+)/gm;
+      const [xPrize, yPrize] = Array.from(lines[2].matchAll(prizeRegex)).map(
+        (match) => match[1]
+      );
+
+      const movementsForA = {
+        x: parseInt(xMovementForA),
+        y: parseInt(yMovementForA),
+      };
+
+      const movementsForB = {
+        x: parseInt(xMovementForB),
+        y: parseInt(yMovementForB),
+      };
+
+      const prizePosition = {
+        x: parseInt(xPrize) + PRIZE_POSITION_EXTRA,
+        y: parseInt(yPrize) + PRIZE_POSITION_EXTRA,
+      };
+
+      return {
+        movementsForA,
+        movementsForB,
+        prizePosition,
+      };
+    })
+    .map((props) => {
+      // Use linear algebra to solve.
+      const xMatrix = [props.movementsForA.x, props.movementsForB.x];
+      const yMatrix = [props.movementsForA.y, props.movementsForB.y];
+
+      const solution = lusolve(
+        [xMatrix, yMatrix],
+        [props.prizePosition.x, props.prizePosition.y]
+      );
+
+      const tokensA = solution[0][0] as number;
+      const tokensB = solution[1][0] as number;
+
+      // Check that both are whole numbers
+      if (
+        Math.abs(tokensA - Math.round(tokensA)) > 0.001 ||
+        Math.abs(tokensB - Math.round(tokensB)) > 0.001
+      ) {
+        return -1;
+      }
+
+      return tokensA * BUTTON_A_TOKENS + tokensB * BUTTON_B_TOKENS;
+    })
+    .reduce((acc, cur) => {
+      if (cur === -1) return acc;
+      return acc + cur;
+    }, 0);
 }
 
 await runSolution(day13b);
