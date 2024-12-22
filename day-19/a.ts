98e7d3bd3f0feac1c9584f3ae84cc5bbb5d93326
kingstarfly <xingxiang@twotreesgroup.com>
2024-12-22 14:13:52 +0800

    19

--- day-19/a.ts
+++ day-19/a.ts
@@ -1,9 +1,38 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day19a(data: string[]) {
-  console.log(data);
-  return 0;
+  const availablePatterns = new Set(data[0].split(", "));
+  const desiredPatterns = data.slice(2).filter((line) => line.length > 0);
+  const maxAvailablePatternLength = Array.from(availablePatterns).reduce(
+    (max, pattern) => Math.max(max, pattern.length),
+    0
+  );
+
+  function canFormPattern(available: Set<string>, desired: string, i: number) {
+    if (i === desired.length) {
+      return true;
+    }
+
+    if (i > desired.length) {
+      return false;
+    }
+
+    for (let j = 0; j < maxAvailablePatternLength; j++) {
+      const patternToCheck = desired.slice(i, i + j + 1);
+      if (available.has(patternToCheck)) {
+        const canForm = canFormPattern(available, desired, i + j + 1);
+        if (canForm) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  return desiredPatterns.filter((desired) =>
+    canFormPattern(availablePatterns, desired, 0)
+  ).length;
 }
 
 await runSolution(day19a);
--- day-19/b.ts
+++ day-19/b.ts
@@ -1,9 +1,54 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day19b(data: string[]) {
-  console.log(data);
-  return 0;
+  const availablePatterns = new Set(data[0].split(", "));
+  const desiredPatterns = data.slice(2).filter((line) => line.length > 0);
+  const maxAvailablePatternLength = Array.from(availablePatterns).reduce(
+    (max, pattern) => Math.max(max, pattern.length),
+    0
+  );
+
+  console.log(
+    availablePatterns.size,
+    desiredPatterns.length,
+    maxAvailablePatternLength
+  );
+
+  const cache = new Map<string, number>();
+
+  function calcNumWaysToFormPattern(desired: string, i: number) {
+    if (i === desired.length) {
+      return 1;
+    }
+
+    if (i > desired.length) {
+      return 0;
+    }
+
+    if (cache.has(`${desired},${i}`)) {
+      return cache.get(`${desired},${i}`)!;
+    }
+
+    let numWays = 0;
+    for (let j = 0; j < maxAvailablePatternLength; j++) {
+      const patternToCheck = desired.slice(i, i + j + 1);
+      if (availablePatterns.has(patternToCheck)) {
+        numWays += calcNumWaysToFormPattern(desired, i + j + 1);
+      }
+    }
+
+    if (i == 0) {
+      console.log(desired, numWays);
+    }
+
+    cache.set(`${desired},${i}`, numWays);
+    return numWays;
+  }
+
+  return desiredPatterns
+    .map((desired) => calcNumWaysToFormPattern(desired, 0))
+    .reduce((a, b) => a + b, 0);
 }
 
 await runSolution(day19b);
