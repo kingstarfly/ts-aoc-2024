0515c9aeaf226be66e2a043715184ff733e342e7
kingstarfly <xingxiang@twotreesgroup.com>
2024-12-28 23:27:52 +0800

    22

--- day-22/a.ts
+++ day-22/a.ts
@@ -1,9 +1,59 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day22a(data: string[]) {
-  console.log(data);
-  return 0;
+  // Mix -> XOR
+  // Prune -> modulo 2^24 -> & 0xFFFFFF
+
+  // Step 1: * 2^6 (left shift by 5) then XOR
+  // Step 2: / 2^5 (right shift by 5) then XOR then modulo 2^24
+  // Step 3: Multiply by 2^11 then mix then prune.
+
+  function stepOne(x: number) {
+    const product = x << 6;
+    const xor = product ^ x;
+    const modulo = xor & 0xffffff;
+    return modulo;
+  }
+
+  function stepTwo(x: number) {
+    const product = x >> 5;
+    const xor = product ^ x;
+    const modulo = xor & 0xffffff;
+    return modulo;
+  }
+
+  function stepThree(x: number) {
+    const product = x << 11;
+    const xor = product ^ x;
+    const modulo = xor & 0xffffff;
+    return modulo;
+  }
+
+  function run(x: number) {
+    let result = x;
+    result = stepOne(result);
+    result = stepTwo(result);
+    result = stepThree(result);
+    return result;
+  }
+
+  function generateFinalSecretNumber(x: number) {
+    const ITERATIONS = 2000;
+    for (let i = 0; i < ITERATIONS; i++) {
+      x = run(x);
+    }
+    return x;
+  }
+
+  let sum = 0;
+  for (const line of data) {
+    const x = parseInt(line);
+    const finalSecretNumber = generateFinalSecretNumber(x);
+    console.log(finalSecretNumber);
+    sum += finalSecretNumber;
+  }
+  return sum;
 }
 
 await runSolution(day22a);
--- day-22/b.ts
+++ day-22/b.ts
@@ -1,9 +1,95 @@
-import { runSolution } from '../utils.ts';
+import { Deque } from "@datastructures-js/deque";
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day22b(data: string[]) {
-  console.log(data);
-  return 0;
+  // map each set of 4 price changes to sum of prices at that time.
+  // But you must do for all buyers.
+  // o(2000 * n) where n is the number of buyers.
+
+  function stepOne(x: number) {
+    const product = x << 6;
+    const xor = product ^ x;
+    const modulo = xor & 0xffffff;
+    return modulo;
+  }
+
+  function stepTwo(x: number) {
+    const product = x >> 5;
+    const xor = product ^ x;
+    const modulo = xor & 0xffffff;
+    return modulo;
+  }
+
+  function stepThree(x: number) {
+    const product = x << 11;
+    const xor = product ^ x;
+    const modulo = xor & 0xffffff;
+    return modulo;
+  }
+
+  const changesToTotalPrice = new Map<string, number>();
+
+  function run(x: number) {
+    let result = x;
+    result = stepOne(result);
+    result = stepTwo(result);
+    result = stepThree(result);
+
+    return result;
+  }
+
+  function getPriceFromSecretNumber(x: number) {
+    return Number(x.toString().split("").pop());
+  }
+
+  function generateFinalSecretNumber(x: number) {
+    const ITERATIONS = 2000;
+    const prevPrices = new Deque([getPriceFromSecretNumber(x)]);
+    const seenChangesStrings = new Set<string>();
+
+    for (let i = 0; i < ITERATIONS; i++) {
+      const result = run(x);
+      x = result;
+
+      const newPrice = getPriceFromSecretNumber(result);
+
+      prevPrices.pushBack(getPriceFromSecretNumber(newPrice));
+      if (prevPrices.size() > 5) {
+        prevPrices.popFront();
+      }
+
+      if (prevPrices.size() === 5) {
+        const prevNumbersArray = prevPrices.toArray();
+        const changesString = prevNumbersArray
+          .slice(1)
+          .map((n, i) => `${n - prevNumbersArray[i]}`)
+          .join(",");
+
+        if (!seenChangesStrings.has(changesString)) {
+          changesToTotalPrice.set(
+            changesString,
+            (changesToTotalPrice.get(changesString) ?? 0) + newPrice
+          );
+          seenChangesStrings.add(changesString);
+        }
+      }
+    }
+    return x;
+  }
+
+  for (const line of data) {
+    const x = parseInt(line);
+    generateFinalSecretNumber(x);
+  }
+
+  // console.log({ changesToTotalPrice });
+  // console.log(changesToTotalPrice.get("-2,1,-1,3"));
+
+  // Scan through the map to find the greatest value.
+  return Array.from(changesToTotalPrice.values()).reduce((a, b) =>
+    Math.max(a, b)
+  );
 }
 
 await runSolution(day22b);
