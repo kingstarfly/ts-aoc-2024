de08a130254efd4724c20e3247064b32145a021a
kingstarfly <xingxiang@twotreesgroup.com>
2024-12-29 20:31:04 +0800

    23

--- day-23/a.ts
+++ day-23/a.ts
@@ -1,9 +1,67 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day23a(data: string[]) {
-  console.log(data);
-  return 0;
+  // Maintain set of computer -> Connected computers (set)
+
+  // After going through all connections, iterate through keys of map.
+  // For each computer, select 2 connections to form the set of 3 and check if all are all connected + start with T.
+
+  const computerToConnectedComputers = new Map<string, Set<string>>();
+
+  for (const line of data) {
+    const [computer1, computer2] = line.split("-");
+
+    const computer1ConnectedComputers =
+      computerToConnectedComputers.get(computer1) ?? new Set();
+    computer1ConnectedComputers.add(computer2);
+    computerToConnectedComputers.set(computer1, computer1ConnectedComputers);
+
+    const computer2ConnectedComputers =
+      computerToConnectedComputers.get(computer2) ?? new Set();
+    computer2ConnectedComputers.add(computer1);
+    computerToConnectedComputers.set(computer2, computer2ConnectedComputers);
+  }
+
+  console.log(computerToConnectedComputers);
+
+  const validSets = new Set<string>();
+  for (const [
+    computer,
+    connectedComputers,
+  ] of computerToConnectedComputers.entries()) {
+    const connectedComputersArray = Array.from(connectedComputers);
+
+    for (let i = 0; i < connectedComputersArray.length; i++) {
+      for (let j = i + 1; j < connectedComputersArray.length; j++) {
+        if (
+          computerToConnectedComputers
+            .get(connectedComputersArray[i])
+            ?.has(connectedComputersArray[j])
+        ) {
+          const computers = [
+            computer,
+            connectedComputersArray[i],
+            connectedComputersArray[j],
+          ];
+          const computersInString = computers.sort().join(",");
+
+          validSets.add(computersInString);
+        }
+      }
+    }
+  }
+
+  let count = 0;
+
+  for (const setString of validSets) {
+    const computers = setString.split(",");
+    if (computers.some((computer) => computer.startsWith("t"))) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 await runSolution(day23a);
--- day-23/b.ts
+++ day-23/b.ts
@@ -1,9 +1,76 @@
-import { runSolution } from '../utils.ts';
+import { runSolution } from "../utils.ts";
 
 /** provide your solution as the return of this function */
 export async function day23b(data: string[]) {
-  console.log(data);
-  return 0;
+  // BK algorithm to enumerate maximal cliques. Cliques are a set of vertices that are all connected to each other.
+  // https://youtu.be/j_uQChgo72I?t=905
+
+  const computerToConnectedComputers = new Map<string, Set<string>>();
+
+  for (const line of data) {
+    const [computer1, computer2] = line.split("-");
+    if (computer1 === undefined || computer2 === undefined) {
+      throw new Error("Invalid line: " + line);
+    }
+
+    const computer1ConnectedComputers =
+      computerToConnectedComputers.get(computer1) ?? new Set();
+    computer1ConnectedComputers.add(computer2);
+    computerToConnectedComputers.set(computer1, computer1ConnectedComputers);
+
+    const computer2ConnectedComputers =
+      computerToConnectedComputers.get(computer2) ?? new Set();
+    computer2ConnectedComputers.add(computer1);
+    computerToConnectedComputers.set(computer2, computer2ConnectedComputers);
+  }
+
+  // Enumerate all maximal cliques.
+  const maximalCliques = new Set<string>();
+  let maximumCliqueSize = 0;
+
+  function enumerateMaximalCliques(
+    currentClique: string[],
+    candidateComputers: Set<string>,
+    visitedComputers: Set<string>
+  ) {
+    if (candidateComputers.size === 0 && visitedComputers.size === 0) {
+      // We found a maximal clique.
+      if (currentClique.length > maximumCliqueSize) {
+        maximumCliqueSize = currentClique.length;
+        maximalCliques.clear();
+        const sortedClique = currentClique.sort();
+        maximalCliques.add(sortedClique.join(","));
+      }
+      return;
+    }
+
+    for (const computer of candidateComputers.keys()) {
+      const updatedClique = currentClique.concat(computer);
+
+      const updatedCandidateComputers = candidateComputers.intersection(
+        computerToConnectedComputers.get(computer) ?? new Set<string>()
+      );
+      const updatedVisitedComputers = visitedComputers.intersection(
+        computerToConnectedComputers.get(computer) ?? new Set<string>()
+      );
+
+      enumerateMaximalCliques(
+        updatedClique,
+        updatedCandidateComputers,
+        updatedVisitedComputers
+      );
+      candidateComputers.delete(computer);
+      visitedComputers.add(computer);
+    }
+  }
+
+  enumerateMaximalCliques(
+    [],
+    new Set(computerToConnectedComputers.keys()),
+    new Set()
+  );
+
+  return maximalCliques.values().next().value;
 }
 
 await runSolution(day23b);
